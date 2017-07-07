9f62ae3d3e7844df52b0c9530cac1ca6e93b4459
Imran Nababan <imzers@gmail.com>
2017-07-07 10:06:46 +0700

    Add files via upload

---
+++ src/TrueMoney/Api/Execute.php
@@ -0,0 +1,150 @@
+<?php
+namespace TrueMoneyWallet\Api;
+class Execute {
+	public $headers;
+	public $response;
+	
+	function __construct($post_method, $post_url, $post_header, $post_field, $post_timeout) {
+		$this->post_method = $post_method;
+		$this->post_url = $post_url;
+		$this->post_field = $post_field;
+		$this->post_timeout = $post_timeout;
+		$this->post_header = $post_header;
+		$this->headers = $this->createHeaders($this->post_header);
+	}
+	function make_curl() {
+		$this->response = $this->create_curl_request($this->post_method, $this->post_url, 'TDP/Api.Context', $this->headers, $this->post_field, $this->post_timeout);
+		return $this->response;
+	}
+	
+	
+	
+	
+	public function collect_headers($headers = array()) {
+		$out = array();
+		if (count($headers) > 0) {
+			foreach ($headers as $keval) {
+				if (is_string($keval)) {
+					if (0 === strpos($keval, 'application/json')) {
+						if (is_array($this->post_field)) {
+							$this->post_field = json_encode($this->post_field, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+						}
+					}
+				}
+			}
+		}
+		return $out;
+	}
+	
+	
+	## $this->create_curl_request('POST', 'http://server.tld/query', 'TDP/Api.Context', $headers = array(), $params = array(), $timeout = 30)
+	function create_curl_request($action, $url, $UA, $headers = null, $params = array(), $timeout = 30) {
+		$cookie = isset($_COOKIE) ? $_COOKIE : array();
+		$url = str_replace( "&amp;", "&", urldecode(trim($url)) );
+		$ch = curl_init();
+		switch (strtolower($action)) {
+			case 'get':
+				if ((is_array($params)) && (count($params) > 0)) {
+					$Querystring = http_build_query($params);
+					$url .= "?";
+					$url .= $Querystring;
+				}
+			break;
+			case 'post':
+			default:
+				$url .= "";
+			break;
+		}
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_VERBOSE, true);
+		if ($headers != null) {
+			curl_setopt($ch, CURLOPT_HEADER, true);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		} else {
+			curl_setopt($ch, CURLOPT_HEADER, false);
+		}
+		curl_setopt($ch, CURLOPT_USERAGENT, $UA);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		//curl_setopt($ch, CURLOPT_COOKIEJAR, $cookie);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+		curl_setopt($ch, CURLOPT_ENCODING, "");
+		curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+		curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
+		curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
+		$post_fields = NULL;
+		switch (strtolower($action)) {
+			case 'get':
+				curl_setopt($ch, CURLOPT_POST, false);
+				curl_setopt($ch, CURLOPT_POSTFIELDS, null);
+				curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+			break;
+			case 'post':
+			default:
+				if ((is_array($params)) && (count($params) > 0) && (is_array($headers) && count($headers) > 0)) {
+					foreach ($headers as $heval) {
+						$getContentType = explode(":", $heval);
+						if (strtolower($getContentType[0]) !== 'content-type') {
+							continue;
+						}
+						switch (strtolower(trim($getContentType[0]))) {
+							case 'content-type':
+								if (isset($getContentType[1])) {
+									switch (strtolower(trim($getContentType[1]))) {
+										case 'application/json':
+											$post_fields = json_encode($params, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+										break;
+										case 'application/x-www-form-urlencoded':
+											$post_fields = http_build_query($params);
+										break;
+										default:
+											$post_fields = http_build_query($params);
+										break;
+									}
+								}
+							break;
+							default:
+								$post_fields = http_build_query($params);
+							break;
+						}
+					}
+				} else if ((!empty($params)) || ($params != '')) {
+					$post_fields = $params;
+				}
+				curl_setopt($ch, CURLOPT_POST, true);
+				curl_setopt($ch, CURLOPT_POSTFIELDS, $post_fields);
+			break;
+		}
+		// Get Response
+		$response = curl_exec($ch);
+		$mixed_info = curl_getinfo($ch);
+		$header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+		$header = substr($response, 0, $header_size);
+		$body = substr($response, $header_size);
+		curl_close ($ch);
+		$return = array(
+			'request'		=> array(
+				'method'			=> "{$action}: {$url}",
+				'header'			=> $headers,
+				'body'				=> $post_fields,
+			),
+			'response'		=> array(),
+		);
+		if (!empty($response) || $response != '') {
+			$return['response']['header'] = array('size' => $header_size, 'content' => $header);
+			$return['response']['body'] = $body;
+			return $return;
+		}
+		return false;
+	}
+	
+	private function createHeaders($headers = array()) {
+		$curlheaders = array();
+		foreach ($headers as $ke => $val) {
+			$curlheaders[] = "{$ke}: {$val}";
+		}
+		return $curlheaders;
+	}
+}
--- src/TrueMoney/Api/Request.php
+++ src/TrueMoney/Api/Request.php
@@ -1,6 +1,67 @@
 <?php
 namespace TrueMoneyWallet\Api;
+
+use TrueMoneyWallet\Api\Execute;
 class Request {
+	public $request_header = array();
+	public $Execute;
 	
 	
+
+	function create_api_execute($base_url, $post_field) {
+		$this->set_method('POST');
+		$this->set_url($base_url);
+		$this->set_post_field($post_field);
+		$this->set_post_header($this->request_header);
+		$this->set_post_timeout(30);
+		$this->Execute = new Execute($this->method, $this->url, $this->post_header, $this->post_field, $this->post_timeout);
+		return $this;
+	}
+	
+	
+	function create_request_header($params = NULL) {
+		$this->set_header('Content-Type', 'application/json');
+		if (isset($params)) {
+			if ((is_array($params)) && (count($params) > 0)) {
+				foreach ($params as $key => $val) {
+					$this->set_header($key, $val);
+				}
+			}
+			
+		}
+	}
+	function set_header($key, $val) {
+		$this->request_header[$key] = $val;
+		return $this;
+	}
+	function set_method($method) {
+		$this->method = $method;
+		return $this;
+	}
+	function set_url($url) {
+		$this->url = "{$url}/payments/v1/payment";
+		return $this;
+	}
+	function set_post_header($headers = null) {
+		if (!isset($headers)) {
+			$this->post_header = array(
+				'Content-type' 	=> 'application/json',
+				'Accept'		=> 'application/json',
+			);
+		}
+		if (is_array($headers) && (count($headers) > 0)) {
+			foreach ($headers as $key => $val) {
+				$this->post_header[$key] = $val;
+			}
+		}
+		return $this;
+	}
+	function set_post_field($post_field) {
+		$this->post_field = $post_field;
+		return $this;
+	}
+	function set_post_timeout($post_timeout = 30) {
+		$this->post_timeout = $post_timeout;
+		return $this;
+	}
 }
