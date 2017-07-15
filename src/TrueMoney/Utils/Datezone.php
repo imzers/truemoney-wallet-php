2e3a806027a6749061cf3eec117068fd84031109
Imran Nababan <imzers@gmail.com>
2017-07-15 18:40:49 +0700

    Create Datezone utils

---
+++ src/TrueMoney/Utils/Datezone.php
@@ -0,0 +1,17 @@
+<?php
+namespace TrueMoneyWallet\Utils;
+class Datezone {
+	protected $DateObject;
+	
+	function __construct($timezone) {
+		// $timezone : Asia/Bangkok
+		$microtime = microtime(true);
+		$micro = sprintf("%06d",($microtime - floor($microtime)) * 1000000);
+		$this->DateObject = new DateTime(date('Y-m-d H:i:s.'.$micro, $microtime));
+		$this->DateObject->setTimezone(new DateTimeZone($timezone));
+	}
+	function create_datetime_format($format) {
+		// $format : YmdHisu
+		return $this->DateObject->format($format);
+	}
+}
