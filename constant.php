2ce048d17f8ad72262fb1d4f6d9837c5ad1f5afb
Imran Nababan <imzers@gmail.com>
2017-08-01 11:11:08 +0700

    Create constant.php

---
+++ constant.php
@@ -0,0 +1,14 @@
+<?php
+class ConstantConfig {
+	const THIS_SERVER_VHOST				= '/path/to/your/vhost/www'; // Server vhost path
+	const THIS_SERVER_NAME 				= 'payment.your-hostname.tld'; // Change domain of live or sandbox
+	const THIS_SERVER_DOMAIN 			= 'payment.your-domain.tld'; // Change domain of live or sandbox
+	const THIS_SERVER_MODE 				= 'sandbox'; // 'sandbox' || 'live'
+	const LOCAL_SERVER_PROTOCOL			= 'http'; // 'http' || 'https'
+	const PUBLIC_URL_PROTOCOL			= 'https';
+	const PUBLIC_URL_ADDRESS			= 'payment-myarena.goodgames.net';
+	const PUBLIC_URL_PATH				= '/payment/##method##/##transaction##';
+	## Another constant you can put below:
+	
+	
+}
