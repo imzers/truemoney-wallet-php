baefbb21af6eabf2bd0707992919b1c5be6ee084
Imran Nababan <imzers@gmail.com>
2017-08-27 21:21:57 +0700

    Create Mac.php

---
+++ src/TrueMoneyWallet/Utils/Mac.php
@@ -0,0 +1,31 @@
+<?php
+namespa
+
+class Mac {
+  protected $value;
+  function __construct() {
+    $this->value = $this->getMac();
+  }
+  private function getMac() {
+	    $configs = '';
+	    $match = $mac = array();
+      if(PHP_OS == 'Linux'){
+        exec('ifconfig',$configs);
+        foreach ($configs as $config){
+          preg_match('/ethernet\s*hwaddr(.*)/i', $config,$match);
+          if(!empty($match)){
+            preg_match('/[0-9A-Za-z:]+/', $match[1],$mac);
+            return str_replace(':', '', $mac[0]);
+          }
+        }
+      } elseif(PHP_OS == 'WINNT'){
+        exec('ipconfig /all',$configs);
+        foreach ($configs as $config){
+          preg_match('/[0-9A-Z]{2}-[0-9A-Z]{2}-[0-9A-Z]{2}-[0-9A-Z]{2}-[0-9A-Z]{2}-[0-9A-Z]{2}/i', $config,$match);
+          if(!empty($match)){
+            return  str_replace('-', '', $match[0]);
+          }
+        }
+      }
+    }
+}
