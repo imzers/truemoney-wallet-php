c5dcfe96bc9943d2254c590494511657f119a062
Imran Nababan <imzers@gmail.com>
2018-01-03 10:53:02 +0700

    Update constant.php

--- constant.php
+++ constant.php
@@ -6,7 +6,7 @@ class ConstantConfig {
 	const THIS_SERVER_MODE 				= 'sandbox'; // 'sandbox' || 'live'
 	const LOCAL_SERVER_PROTOCOL			= 'http'; // 'http' || 'https'
 	const PUBLIC_URL_PROTOCOL			= 'https';
-	const PUBLIC_URL_ADDRESS			= 'payment-myarena.goodgames.net';
+	const PUBLIC_URL_ADDRESS			= 'payment.your-domain.tld'; // Change domain of live or sandbox
 	const PUBLIC_URL_PATH				= '/payment/##method##/##transaction##';
 	## Another constant you can put below:
 	
